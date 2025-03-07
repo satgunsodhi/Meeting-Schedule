7e8798a12fa2fedb9fa3ae71f86540104b286f1f
satgunsodhi <32atsatgun@gmail.com>
2025-03-07 21:40:35 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from '@chakra-ui/react'
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+    <Button>Hello</Button>
+    </>
+  )
+}
+
+export default App
